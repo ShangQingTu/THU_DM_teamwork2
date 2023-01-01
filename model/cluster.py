b9e657c61018413c3d2c21116aa966be15df1c3d
涂尚卿 <1103435855@qq.com>
2023-01-01 10:20:44 +0800

    vdm for distance

--- model/cluster.py
+++ model/cluster.py
@@ -8,7 +8,7 @@ nominal_cols = ["race", "gender", "admission_type_id", "discharge_disposition_id
 
 
 class Calculator:
-    def __init__(self, df, k):
+    def __init__(self, df, k, data):
         self.k = k
         self.numerical_ids = []
         self.nominal_ids = []
@@ -18,14 +18,38 @@ class Calculator:
                 self.nominal_ids.append(i)
             else:
                 self.numerical_ids.append(i)
+        self.id2count_split = {}  # key: cluster_id, value: a dict like self.id2count_merge
+        self.id2count_merge = {}  # key: column_id, value: Dict of Counter, Counter: {data's value :count}
+        self.init_count(data)
+        # 　假设一开始都在一个cluster上
+        self.update_count({0: data})
+
+    def init_count(self, data):
+        # 初始化总体的counter
+        for col_id in self.nominal_ids:
+            unique, counts = np.unique(data[:, col_id], return_counts=True)
+            count_merge = {}
+            for i, key in enumerate(unique):
+                count_merge[key] = counts[i]
+            self.id2count_merge[col_id] = count_merge
+
+    def update_count(self, cluster_dict):
+        # 每轮更新每个cluster上的counter
+        for cluster_id, _data in cluster_dict.items():
+            id2count_merge = {}
+            for col_id in self.nominal_ids:
+                unique, counts = np.unique(_data[:, col_id], return_counts=True)
+                count_merge = {}
+                for i, key in enumerate(unique):
+                    count_merge[key] = counts[i]
+                id2count_merge[col_id] = count_merge
+            self.id2count_split[cluster_id] = id2count_merge
 
     def get_matrix_split(self, u, node, i):
-        # TODO
-        return 0
+        return self.id2count_split[i][u][node[u]]
 
     def get_matrix_merge(self, u, node):
-        # TODO
-        return 0
+        return self.id2count_merge[u][node[u]]
 
     def cal_distance(self, node, center):
         # 　对于连续有序的numerical属性, 用闵可夫斯基距离
@@ -35,8 +59,8 @@ class Calculator:
         for u in self.nominal_ids:
             for i in range(self.k):
                 m_uai = self.get_matrix_split(u, node, i)
-                m_ubi = self.get_matrix_split(u, node, i)
-                m_ua = self.get_matrix_merge(u, center)
+                m_ubi = self.get_matrix_split(u, center, i)
+                m_ua = self.get_matrix_merge(u, node)
                 m_ub = self.get_matrix_merge(u, center)
                 vdm += (m_uai / m_ua - m_ubi / m_ub) ** 2
 
@@ -78,24 +102,38 @@ class KMeans:
             if cluster_class not in cluster_dict.keys():
                 cluster_dict[cluster_class] = []
             cluster_dict[cluster_class].append(node)
-        return cluster_dict
+        # 更新counter
+        np_cluster_dict = {}
+        for k, v in cluster_dict:
+            np_data = np.asarray(v)
+            np_cluster_dict[k] = np_data
+        self.calculator.update_count(np_cluster_dict)
+        return cluster_dict, np_cluster_dict
 
-    def get_avg_center(self, cluster_dict):
+    def get_avg_center(self, np_cluster_dict):
         new_center = []
         for i in range(self.k):
-            # TODO
-            # 对于类别属性,选出现次数最多的 而非简单平均
-            center = np.mean(cluster_dict[i], axis=0)
+            # 初始化一个点
+            center = np_cluster_dict[i][0]
+            # 算平均
+            cluster_nodes = np_cluster_dict[i]
+            avg = np.mean(cluster_nodes[:, self.calculator.numerical_ids], axis=0)
+            center[self.calculator.numerical_ids] = avg
+            # 对于类别属性,选出现次数最多的 而非简单平均 (TODO　这种做法有待商榷)
+            for col_id in self.calculator.nominal_ids:
+                # np.unique默认是降序
+                unique = np.unique(cluster_nodes[:, col_id])
+                center[col_id] = unique[0]
             new_center.append(center)
         return new_center
 
     def predict(self, df):
         # 数据准备
-        self.calculator = Calculator(df, self.k)
         data = df.to_numpy()
+        self.calculator = Calculator(df, self.k, data)
         # 初始随机选中心
         center = self.random_center(data)
-        cluster_dict = self.get_cluster(data, center)
+        cluster_dict, np_cluster_dict = self.get_cluster(data, center)
         new_variance = self.calculator.cal_variance(cluster_dict, center)
         old_variance = 1
         _iter = 0
@@ -104,8 +142,8 @@ class KMeans:
             if _iter >= self.max_iter:
                 break
             # 重新选平均值点作为簇中心
-            center = self.get_avg_center(cluster_dict)
-            cluster_dict = self.get_cluster(data, center)
+            center = self.get_avg_center(np_cluster_dict)
+            cluster_dict, np_cluster_dict = self.get_cluster(data, center)
             old_variance = new_variance
             new_variance = self.calculator.cal_variance(cluster_dict, center)
             _iter += 1
@@ -132,7 +170,9 @@ class DBSCAN:
 
 
 if __name__ == '__main__':
-    df_data = pd.DataFrame([[1, 1, 1], [2, 2, 2], [1, 2, 1], [9, 8, 7], [7, 8, 9], [8, 9, 7]], columns=['a', 'b', 'c'])
+    df_data = pd.DataFrame(
+        [[1, 1, 'a', 'c'], [2, 2, 'a', 'd'], [1, 2, 'a', 'a'], [9, 8, 'a', 'a'], [7, 8, 'b', 'a'], [8, 9, 'b', 'a']],
+        columns=['a', 'b', 'insulin', 'glimepiride'])
     print(df_data)
     clf = KMeans(2, 114)
     cluster_dict1, center1 = clf.predict(df_data)
